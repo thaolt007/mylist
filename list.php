3ec807fe382ce96792a33430267bacbad38fc03d
thaolt <thaolt117@gmail.com>
2017-01-13 00:35:59 +0700

    chuyển html sang php, thêm xử lý, xóa list.html sau khi chuyển

---
+++ list.php
@@ -0,0 +1,98 @@
+<!DOCTYPE html5>
+<head>
+    <title>My list</title>
+    <link rel="stylesheet" type="text/css" href="css/style.css" />
+    <script type="text/javascript" src="js/jquery-1.11.3.min.js"></script>
+    <style type="text/css">
+    </style>
+</head>
+<?php
+    require 'process.php';
+?>
+<body>
+    <div id="wrap">
+        <div id="top">
+            <div id="input-link">
+                <div class="left"><input id="link" type="text" /></div>
+                <div class="right"><input id="submit-but" type="submit" value="OK" /> </div>
+            </div>
+        </div>
+        <!--End id top--></!--End>
+        <div class="break-line"></div>
+        <div id="mid">
+        <?php
+        foreach ($data as $row) {
+            echo "<div class='link-row'>";
+            echo "<div class='left'>";
+
+            if (strpos($row, "http")!== FALSE) {
+                // hyperlink
+                $arr = explode(" ", $row);
+                echo '<a href="'.$arr[0].'" target="_blank">'.$arr[0].'</a>';
+                echo "</div>";
+                echo "<div class='right'>";
+                echo "<p>".$arr[1]."</p>";
+                echo "</div>";
+            } else {
+                // string text
+                
+                $arr = array();
+                $t = strrpos($row, " ");
+                $arr[] = substr($row,0,  $t);
+                $arr[] = substr($row,$t);
+                echo '<a href="http://google.com/?q='.$arr[0].'" target="_blank">'.$arr[0].'</a>';
+                echo "</div>";
+                echo "<div class='right'>";
+                echo "<p>".$arr[1]."</p>";
+                echo "</div>";
+            }
+            echo "</div>";
+            echo "<div class='break-line'></div>";
+        }
+        ?>
+        </div>
+        <!--End id mid--></!--End>
+        <div id="bot">
+            <div id="navi">
+                <div class="left">
+                    <button>Previous</button>
+                    <button>Next</button>
+                </div>
+                <div class="right">
+                    <a href="#top"><button>TOP</button></a>
+                </div>
+            </div>
+        </div>
+        <!--End id bot--></!--End>
+    </div>
+    <!--End id wrap--></!--End>
+    <script>
+        $('#submit-but').click(function (e) {
+            e.preventDefault(); //loai bo cac hanh dong mac dinh
+            
+            var link = $('#link').val();
+            $.ajax({
+                        type: 'POST', // method
+                        url: './process.php', // action
+                        dataType: 'JSON',// kieu du lieu nhan ve
+                        data: {// data gui cung request
+                            link: link,
+                        },
+                        success: function (data) {// thanh cong status http = 200
+                            // in ket qua sang tab console
+                            console.log(data);
+                            
+                            // xu ly du lieu tra ve
+                            if (data.code === 1) {
+                                
+                            } else {
+                                
+                            }
+                            
+                        }
+                    });
+        });
+    </script>
+</body>
+
+</html>
---
+++ process.php
@@ -0,0 +1,57 @@
+<?php
+//Ham ghi ra file
+function writelist($record) {
+    $filename = "link.txt";
+    $file = fopen($filename, "a");
+
+    if ($file == false) {
+        echo "Loi mo file khi write";
+        return FALSE;
+    }
+    fwrite($file, $record);
+    fclose($file);
+    return TRUE;
+}
+
+//Ham doc tu file
+function readlist() {
+    $filename = "link.txt";
+    $data = array();
+    if (file_exists($filename)) {
+        $file = fopen($filename, "r");
+        if ($file == false) {
+            echo "Loi mo file khi read";
+            return 0;
+        }
+
+        while (!feof($file)) {
+            $record = fgets($file); // get 1 row
+            $data[] = $record;
+        }
+    }
+    return $data;
+}
+
+//#############################################################################
+//define(FAIL, 0);
+//define(SUCCESS, 1);
+
+$link = $_POST['link'];
+$day = date("d-m-Y");
+
+//Xu ly ghi ra file
+$response = array();
+$response['code'] = 0;
+if ($link != "") {
+    $record = $link . " " . $day . "\n";
+    $status = writelist($record);
+    if ($status) {
+        $response['code'] = 1;
+    }
+}
+
+//Xu ly doc tu file
+$data = readlist();
+
+
+?>
